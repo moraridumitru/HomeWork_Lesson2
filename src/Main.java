b5d29ef7b071eb43750d6a3e3771512078d238fe
moraridumitru <133877747+moraridumitru@users.noreply.github.com>
2023-05-23 16:24:32 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+    /** это простое количество элементов в классе Java Company_Array с использованием циклов if и else if**/
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] company = {"MAIB", "Orange", "Moldtelecom", "Endava", "Moldcell","Unite"};
+
+        // Подсчет количества элементов массива
+        int arrayLength = company.length;
+
+        // Вывод последнего элемента массива
+        String lastElement = company[arrayLength - 1];
+
+        // Вывод первого элемента массива
+        String firstElement = company[0];
+
+        // Вывод второго элемента массива
+        String secondElement = company[1];
+
+        // Вывод размера массива
+        System.out.println("Количество элементов массива: " + arrayLength);
+        System.out.println("Последний элемент: " + lastElement);
+        System.out.println("Первый элемент: " + firstElement);
+        System.out.println("Второй элемент: " + secondElement);
     }
 }
